ef2caf1d0defec3400182a49c9c7c3f72e6b53db
Ibr <ibrahmad18@gmail.com>
2024-10-10 19:56:07 +0100

    adding analysedata1

--- analyse_data/analyse_participant.py
+++ analyse_data/analyse_participant.py
@@ -1,4 +1,20 @@
-# On donne la lliste de ces participant parcourir la liste et afficher une salutation a chaque utilsateur 
+# On donne la lliste de ces participant
+# parcourir la liste et afficher une salutation a chaque utilsateur
 # Utiliser Salam Aleykoum Mr pour les masculin et Salam Mme [nom] pour les femmes
 # Afficher enfin leur ages respective
-# parts = [ ["Ali", 18, , "M"], ["Moussa", 25, "M"], ["Mariama", 42, "F"]  ,["Fatouma", 15, "F"]
+
+parts = [
+    ["Ali", 18, "M"],
+    ["Mariama", 42, "F"],
+    ["Moussa", 25, "M"],
+    ["Fatouma", 15, "F"],
+]
+
+# parcours de la liste pour saluer par genre
+for part in parts:
+    name, age, genre = part
+
+    if genre == "M":
+        print(f"Salam aleykoum Mr {name} vous etes agee de {age}")
+    else:
+        print(f"Salam Aleykoum Mme {name} agee de {age}")
